b2a5463804781e7da6ab64329e86cb8b54d40eef
pjcevans <peterjcevans@gmail.com>
2017-10-26 13:42:17 +0100

    interpreted json data structure into bootstrap grid

--- src/components/ToDoApp.js
+++ src/components/ToDoApp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ToDoTitle from './ToDoTitle.js';
-import { Row, Grid} from 'react-bootstrap';
+import { Row, Grid, Col } from 'react-bootstrap';
+import './ToDoApp.css';
 
 const ToDoApp = (props) => {
   const todosList = [
@@ -47,37 +48,49 @@ const ToDoApp = (props) => {
   ];
   return(
     <Grid>
-      <Row>
-          <p>Hello this is a ToDo App</p>
-      </Row>
-
-      <Row>
-        <ToDoTitle />
-      </Row>
 
       {/* For each category item, render the title, outcomes & loop over the todos */}
       {todosList.map((category) => {
         return (
-          <Row>
-            <p>Title: {category.title}</p>
+          <div>
+            <Col xs={12} md={8} className="category">
+              <p className="category-text">Title: {category.title}</p>
 
-            {/* For each ToDo render the title and loop over the sub-ToDos */}
-            {category.todos.map((todo) => {
-              return (
-                <div>
-                  <p>Todo: {todo.title}</p>
+              {/* For each ToDo render the title and loop over the sub-ToDos */}
+              {category.todos.map((todo) => {
+                return (
+                  <div>
+                    <Row>
+                      <Col className="todo" xs={11} md={11}>
+                        <p className="todo-text">Todo: {todo.title}</p>
+                      </Col>
+                      <Col xs={1} md={1}>
+                        <p  className="todo-text">X</p>
+                      </Col>
+                    </Row>
 
-                  {/* Loop over the sub-ToDos */}
-                  {todo.subtodos.map((subtodo) => {
-                    return <p>{subtodo.title}</p>
-                  })}
+                    {/* Loop over the sub-ToDos */}
+                    {todo.subtodos.map((subtodo) => {
+                      return (
+                        <Row>
+                          <Col className="subtodo" xs={11} md={11}>
+                            <p className="subtodo-text">Sub-ToDo: {subtodo.title}</p>
+                          </Col>
+                          <Col xs={1} md={1}>
+                            <p className="subtodo-text">X</p>
+                          </Col>
+                        </Row>
+                      )
+                    })}
 
-                </div>
-              )
-            })}
-
-            <p>Outcomes: {category.outcomes}</p>
-          </Row>
+                  </div>
+                )
+              })}
+            </Col>
+            <Col xs={12} md={4} className="outcomes">
+              <p>Outcomes: {category.outcomes}</p>
+            </Col>
+          </div>
         )
       })}
 
